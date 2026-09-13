ad742e395d1ec403df6c0b9c34142a4f1d42374d
Marcus Fong <249391498+benbenlij@users.noreply.github.com>
2026-09-13 18:35:27 -0500

    fix: add Desktop and Downloads folder in lab 2

--- lab-2/setup.sh
+++ lab-2/setup.sh
@@ -10,6 +10,7 @@ chown -R investigator:investigator /home/investigator
 echo "Name: David
 Last Seen: 5/16/2015" > '/home/investigator/hacker_info.txt'
 # Create Forensics Question
+mkdir '/home/investigator/Desktop'
 echo "ANSWER: " > '/home/investigator/Desktop/Forensics Question 1.txt'
 
 # Objective 2
@@ -17,6 +18,7 @@ touch '/home/investigator/unauthorized_image.jpg'
 
 # Objective 3
 mkdir '/home/investigator/quarantine'
+mkdir '/home/investigator/Downloads'
 touch '/home/investigator/Downloads/download.zip'
 echo "This is critical information" > 'critical_info.txt'
 
