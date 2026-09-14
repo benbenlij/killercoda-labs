e026a8e90ad6060166634eaaedfe4afe3215f1cf
Marcus Fong <249391498+benbenlij@users.noreply.github.com>
2026-09-13 19:40:07 -0500

    fix: fix critical syntax errors in verify scripts

--- lab-2/step1/verify.sh
+++ lab-2/step1/verify.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-FILE_PATH = "/home/investigator/Desktop/Forensics Question 1.txt"
+FILE_PATH="/home/investigator/Desktop/Forensics Question 1.txt"
 
 # Check if file exists
 if [[ ! -f "$FILE_PATH" ]]; then
@@ -10,9 +10,9 @@ fi
 
 # Read and compare answer
 if [[ $(cat "$FILE_PATH") =~ ANSWER:[[:space:]]*(.*) ]]; then
-    user_answer = "${BASH_REMATCH[1]}"
-    user_answer = $(echo "$user_answer" | xargs | tr '[:upper:]' '[:lower:]')
-    if [[ "$user_answer" == "david"]]; then
+    user_answer="${BASH_REMATCH[1]}"
+    user_answer=$(echo "$user_answer" | xargs | tr '[:upper:]' '[:lower:]')
+    if [[ "$user_answer" == "david" ]]; then
         echo "Correct!"
         exit 0
     else 
@@ -21,5 +21,5 @@ if [[ $(cat "$FILE_PATH") =~ ANSWER:[[:space:]]*(.*) ]]; then
     fi
 else
     echo "No line starting with 'ANSWER'."
-    exit 0
+    exit 1
 fi
--- lab-2/step3/verify.sh
+++ lab-2/step3/verify.sh
@@ -3,10 +3,10 @@
 if [ -n "$(ls -A /home/investigator/Downloads)" ]; then
     echo "Remember to rename and move the download"
     exit 1
-elif [ -z "$(ls -A /path/to/dir)" ]; then
+elif [ -z "$(ls -A /home/investigator/quarantine)" ]; then
     echo "Make sure to move the download to the quarantine diredctory"
     exit 1
-elif [[ ! -f "/path/to/file1.txt" || ! -f "/path/to/file2.txt" ]]; then
+elif [[ ! -f "/home/investigator/critical_info.txt" || ! -f "/home/investigator/critical_info.backup" ]]; then
     echo "You must have the original file and its backup"
     exit 1
 else
